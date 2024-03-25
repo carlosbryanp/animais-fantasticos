641ab1207995b78845e92d7e73ab9e43c9da5195
Carlos Paiva <carlosbryanpv@gmail.com>
2024-03-25 15:21:47 -0300

    Projeto final animais fantasticos

---
+++ js/script.js
@@ -0,0 +1,21 @@
+import initScrollSmooth from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initDropdownMenu from "./modules/dropdown.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+
+initScrollSmooth();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initDropdownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBitcoin();
